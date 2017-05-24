a618e5d0edc9fe498b93cfdcf2c34f9d93ae4c3c
tungptvn <tungpt.hd@gmail.com>
2017-05-24 13:57:31 +0700

    feat(*): add user extend

--- Infrastructure/ApplicationUser.cs
+++ Infrastructure/ApplicationUser.cs
@@ -1,8 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace dotnetcore_aurelia_demo.Infrastructure
 {
     public class ApplicationUser : IdentityUser
     {
+        
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+
+        [Required]
+        public byte Level { get; set; }
+
+        [Required]
+        public DateTime JoinDate { get; set; }
     }
 }
