c43c2aa175b1231390a4085197e7ef324c75115a
tungptvn <tungpt.hd@gmail.com>
2017-05-24 11:15:34 +0700

    chore(*): delete model test

--- Infrastructure/Person.cs
+++
@@ -1,9 +0,0 @@
-namespace dotnetcore_aurelia_demo.Infrastructure
-{
-    public class Person
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Age { get; set; }
-    }
-}
