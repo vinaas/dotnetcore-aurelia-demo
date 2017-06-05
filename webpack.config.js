3392247a4e2d46c6f5b93643bf7631a9a284c581
tungptvn <tungpt.hd@gmail.com>
2017-06-05 21:05:28 +0700

    feat(packs): add jquery scroll

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
 const path = require('path');
 const webpack = require('webpack');
-const { AureliaPlugin } = require('aurelia-webpack-plugin');
+const {
+    AureliaPlugin
+} = require('aurelia-webpack-plugin');
 const bundleOutputDir = './wwwroot/dist';
 
 module.exports = (env) => {
     const isDevBuild = !(env && env.prod);
     return [{
-        stats: { modules: false },
-        entry: { 'app': 'aurelia-bootstrapper' },
+        stats: {
+            modules: false
+        },
+        entry: {
+            'app': 'aurelia-bootstrapper'
+        },
         resolve: {
             extensions: ['.ts', '.js'],
             modules: ['ClientApp', 'node_modules'],
@@ -18,24 +24,52 @@ module.exports = (env) => {
             filename: '[name].js'
         },
         module: {
-            rules: [
-                { test: /\.ts$/i, include: /ClientApp/, use: 'ts-loader?silent=true' },
-                { test: /\.html$/i, use: 'html-loader' },
-                { test: /\.css$/i, use: isDevBuild ? 'css-loader' : 'css-loader?minimize' },
-                { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' }
+            rules: [{
+                    test: /\.ts$/i,
+                    include: /ClientApp/,
+                    use: 'ts-loader?silent=true'
+                },
+                {
+                    test: /\.html$/i,
+                    use: 'html-loader'
+                },
+                {
+                    test: /\.css$/i,
+                    use: isDevBuild ? 'css-loader' : 'css-loader?minimize'
+                },
+                {
+                    test: /\.(png|jpg|jpeg|gif|svg)$/,
+                    use: 'url-loader?limit=25000'
+                },
+                {
+                    test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                    loader: "url-loader?limit=10000&mimetype=application/font-woff"
+                },
+                {
+                    test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                    loader: "file-loader"
+                }
             ]
         },
         plugins: [
-            new webpack.DefinePlugin({ IS_DEV_BUILD: JSON.stringify(isDevBuild) }),
+            new webpack.ProvidePlugin({
+                $: 'jquery',
+                jQuery: 'jquery'
+            }),
+            new webpack.DefinePlugin({
+                IS_DEV_BUILD: JSON.stringify(isDevBuild)
+            }),
             new webpack.DllReferencePlugin({
                 context: __dirname,
                 manifest: require('./wwwroot/dist/vendor-manifest.json')
             }),
-            new AureliaPlugin({ aureliaApp: 'boot' })
+            new AureliaPlugin({
+                aureliaApp: 'boot'
+            })
         ].concat(isDevBuild ? [
             new webpack.SourceMapDevToolPlugin({
                 filename: '[file].map', // Remove this line if you prefer inline source maps
-                moduleFilenameTemplate: path.relative(bundleOutputDir, '[resourcePath]')  // Point sourcemap entries to the original file locations on disk
+                moduleFilenameTemplate: path.relative(bundleOutputDir, '[resourcePath]') // Point sourcemap entries to the original file locations on disk
             })
         ] : [
             new webpack.optimize.UglifyJsPlugin()
