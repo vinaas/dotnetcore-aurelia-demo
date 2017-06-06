e39dc910693ad09f9c52e475b9359a90662faa3e
tungptvn <tungpt.hd@gmail.com>
2017-06-06 10:54:21 +0700

    feat(*): add login

--- ClientApp/app/components/login/login.ts
+++ ClientApp/app/components/login/login.ts
@@ -1,3 +1,21 @@
-export class Login{
-
+import { Router } from 'aurelia-router';
+import * as $ from 'jquery';
+import '../../../theme/adminLTE/plugins/iCheck/icheck.js';
+import { autoinject } from "aurelia-dependency-injection";
+@autoinject()
+export class Login {
+    constructor(private router: Router) {
+    }
+    login() {
+        this.router.navigateToRoute('admin')
+    }
+    attached() {
+        $(function () {
+            $('input').iCheck({
+                checkboxClass: 'icheckbox_square-blue',
+                radioClass: 'iradio_square-blue',
+                increaseArea: '20%' // optional
+            });
+        });
+    }
 }
