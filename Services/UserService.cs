2702bca32a3a43151584a12582028614be09b2ab
tungptvn <tungpt.hd@gmail.com>
2017-06-09 16:46:44 +0700

    feat(*): add rolemanager to userService

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,20 +1,27 @@
 using System;
 using dotnetcore_aurelia_demo.Infrastructure;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace dotnetcore_aurelia_demo.Services
 {
     public class UserService : UserServiceInterface
     {
         private readonly UserManager<ApplicationUser> _userManager;
-        public UserService(UserManager<ApplicationUser> userManager)
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public UserService(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
-            this._userManager = userManager;
+            _userManager = userManager;
+            _roleManager = roleManager;
         }
 
         public UserManager<ApplicationUser> GetUserManager()
         {
             return this._userManager;
+        }
+        public RoleManager<IdentityRole> GetRoleManager()
+        {
+            return _roleManager;
         }
     }
 }
