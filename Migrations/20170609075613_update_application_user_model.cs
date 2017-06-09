8d0fb0c7ac11ddbd04f48e1ded1563e00770f489
tungptvn <tungpt.hd@gmail.com>
2017-06-09 15:32:03 +0700

    feat(*): update ApplicationUser model

---
+++ Migrations/20170609075613_update_application_user_model.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dotnetcore_aurelia_demo.Migrations
+{
+    public partial class update_application_user_model : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JoinDate",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Level",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "JoinDate",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<byte>(
+                name: "Level",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+    }
+}
