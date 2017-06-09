d01bad31a9123c00fcff9494f4e45a911c369e9b
tungptvn <tungpt.hd@gmail.com>
2017-06-09 16:47:13 +0700

    chore(*): update userService Interface

--- Services/UserServiceInterface.cs
+++ Services/UserServiceInterface.cs
@@ -1,10 +1,12 @@
 using dotnetcore_aurelia_demo.Infrastructure;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace dotnetcore_aurelia_demo.Services
 {
     public interface UserServiceInterface
     {
         UserManager<ApplicationUser> GetUserManager();
+        RoleManager<IdentityRole> GetRoleManager();
     }
 }
