c54dca650beae72385eb0d9c3f3c4aff63041b85
tungptvn <tungpt.hd@gmail.com>
2017-06-09 10:09:43 +0700

    chore

--- ConfigureServices/MapperConfiguration.cs
+++ ConfigureServices/MapperConfiguration.cs
@@ -2,13 +2,12 @@ using AutoMapper;
 using dotnetcore_aurelia_demo.Infrastructure;
 using dotnetcore_aurelia_demo.Models.AccountViewModels;
 
-namespace dotnetcore_aurelia_demo
+namespace dotnetcore_aurelia_demo.ConfigureServices
 {
     public class MapperConfiguration : Profile
     {
         public MapperConfiguration()
         {
-            // Add as many of these lines as you need to map your objects
             CreateMap<ApplicationUser, LoginResultModel>();
         }
     }
